92cb35d1d1c8d8d8ce6e6b91fc91078cc62a59b9
Alessandrx204 <98517560+Alessandrx204@users.noreply.github.com>
2026-05-07 14:17:03 +0200

    complete rework
    complete rework

---
+++ src/ReapySet/main.py
@@ -0,0 +1,36 @@
+import sys
+from pathlib import Path
+
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import QApplication
+
+from logic_mainwindow import LogicMainWindow
+from config import MwConfig as Mwc
+
+
+def main():
+    app: QApplication = QApplication(sys.argv)
+    #m_window: RsMainWindow = RsMainWindow()
+    m_window = LogicMainWindow()
+    m_window.create_language_buttons(m_window.button_labels_list, Mwc.LangBtnWidget.max_btn_x_row, m_window.main_layout)
+    base_dir : Path = Path(__file__).resolve().parent
+    icon_path : Path = base_dir / "resources" / "icon.png" #sets icon
+
+    icon: QIcon = QIcon(str(icon_path))
+    app.setWindowIcon(icon)
+    m_window.setWindowIcon(icon)
+
+
+    m_window.show()
+    m_window.original_geometry = m_window.geometry()
+    #print("--- DEBUG PERCORSI ---")
+    #print(f"Cartella da cui parte lo script: {os.getcwd()}")
+    #print(f"File presenti in questa cartella: {os.listdir(os.getcwd())}")
+    #print("-----------------------")
+    sys.exit(app.exec())
+
+
+
+if __name__ == "__main__":
+
+    main()
